7d4a8ca73dd6ec20c8bec212fa5213a65bea009a
taff_rodriguez <taff.rodriguez@gmail.com>
2016-01-10 16:01:03 -0600

    Initial Commit

---
+++ PayPal-Manual/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PayPal-Manual
+//
+//  Created by Benjas on 10/01/16.
+//  Copyright © 2016 Benjas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PayPal-Manual/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PayPal-Manual
+//
+//  Created by Benjas on 10/01/16.
+//  Copyright © 2016 Benjas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
